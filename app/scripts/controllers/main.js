d3e66a8309b0d188344156373b09ef7cad51deda
Bruno Marques <bruno@uberwald-dev.net>
2014-05-09 00:04:19 +0000

    Yeoman scaffolding and the whole lot

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('automationTrackBuilderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
