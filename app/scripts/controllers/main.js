9ae3d897a561b7e744a5f0a0cf8ede61b61e1651
Bruno Marques <bomarques@inf.ufrgs.br>
2014-05-10 23:45:11 -0300

    Track importing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -64,6 +64,103 @@ angular.module('automationTrackBuilderApp')
                 overview.setBackground(imgObj);
             };
         });
+        $scope.importFile = loadFile('import-file', function(result) {
+            if (result.substr(0,23) != "data:text/x-lua;base64,")
+                throw Error("Not a Lua script");
+            var script = atob(result.substr(23));
+            var ast = luaparse.parse(script);
+            ast.body.forEach(function(root) {
+                if (root.variables[0].name == "Track") {
+                    var identifiers = {STRAIGHT: 0, LEFT: 1, RIGHT: -1};
+                    var result = {
+                        start: {},
+                        scale: {},
+                        layout: [],
+                        layoutInfo: [],
+                        cornerRadius: [],
+                        slope: [],
+                        sportiness: [],
+                        camber: [],
+                        corners: []
+                    };
+                    root.init[0].fields.forEach(function(field) {
+                        switch(field.key.name) {
+                            case "Name": result.name = field.value.value; break;
+                            case "Split1": result.split1 = field.value.value; break;
+                            case "Split2": result.split2 = field.value.value; break;
+                            case "Start":
+                                result.start = {
+                                    x: field.value.fields[0].value.value,
+                                    y: field.value.fields[1].value.value
+                                };
+                                break;
+                            case "Scale":
+                                if (field.value.operator != '/')
+                                    throw Error("Scale must be a pixels/length division");
+                                result.scale = {
+                                    pixels: field.value.left.value,
+                                    meters: field.value.right.value
+                                };
+                                break;
+                            case "Layout":
+                                field.value.fields.forEach(function(innerField) {
+                                    if (innerField.value.type == "Identifier")
+                                        result.layout.push(identifiers[innerField.value.name]);
+                                    else if (innerField.value.type == "NumericLiteral")
+                                        result.layout.push(innerField.value.value);
+                                });
+                                break;
+                            case "LayoutInfo":
+                                field.value.fields.forEach(function(innerField) {
+                                    result.layoutInfo.push(innerField.value.value);
+                                });
+                                break;
+                            case "CornerRadius":
+                                field.value.fields.forEach(function(innerField) {
+                                    result.cornerRadius.push(innerField.value.value);
+                                });
+                                break;
+                            case "Slope":
+                                field.value.fields.forEach(function(innerField) {
+                                    result.slope.push(innerField.value.value);
+                                });
+                                break;
+                            case "Sportiness":
+                                field.value.fields.forEach(function(innerField) {
+                                    result.sportiness.push(innerField.value.value);
+                                });
+                                break;
+                            case "Camber":
+                                field.value.fields.forEach(function(innerField) {
+                                    result.camber.push(innerField.value.value);
+                                });
+                                break;
+                        }
+                    });
+                    for (var i = 0; i < result.layout.length; i++) {
+                        result.corners.push({
+                            layout: result.layout[i],
+                            layoutInfo: result.layoutInfo[i],
+                            cornerRadius: result.cornerRadius[i],
+                            slope: result.slope[i],
+                            sportiness: result.sportiness[i],
+                            camber: result.camber[i]
+                        });
+                    };
+                    delete result.layout;
+                    delete result.layoutInfo;
+                    delete result.cornerRadius;
+                    delete result.slope;
+                    delete result.sportiness;
+                    delete result.camber;
+                    Object.keys(result).forEach(function(i) {
+                        $scope[i] = result[i];
+                    });
+                    $scope.selected = result.corners.length-1;
+                    $scope.$apply();
+                }
+            });
+        });
 
         $scope.cornerPush = function() {
             var newlen = $scope.corners.push($scope.defaultCorner);
