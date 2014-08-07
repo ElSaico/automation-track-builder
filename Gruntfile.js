d26148f66e810575bf15217147b587c8ceb7ef51
Bruno Marques <bruno@uberwald-dev.net>
2014-08-07 17:00:57 +0000

    Applying uberwald-dev.net's visual identity (i.e. 'ewal' theme)

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 8888,
+        port: 8000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
         livereload: 3572
